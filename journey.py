50023692e6b27bdabdf3f90df71d2f2e16acd68a
Bradley Dowling <bradley.dowling@wsu.edu>
2023-01-12 20:09:34 -0800

    Update journey.py

--- journey.py
+++ journey.py
@@ -1,8 +1,12 @@
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu') 
 service = Service(executable_path="/usr/local/share/chrome_driver/chromedriver")
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(service=service, options=options)
 
 driver.get("https://www.selenium.dev/")
 
